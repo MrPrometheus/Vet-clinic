6fdbc33a4107c5721d9d1a4a5f5b26508ad26568
MrPrometheus <amir505553amir@gmail.com>
2021-05-21 19:21:13 +0300

    fix: Добавлен eslint и pretier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+const App = () => {
+  return <div className="App" />
 }
 
-export default App;
+export default App
